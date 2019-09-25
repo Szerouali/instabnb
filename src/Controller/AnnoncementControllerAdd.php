4a3d7e23594776f06871da8e53411c0c3230e61e
sara30081996 <45093009+sara30081996@users.noreply.github.com>
2019-09-25 14:53:00 +0200

    mon comit

--- src/Controller/AnnoncementControllerAdd.php
+++ src/Controller/AnnoncementControllerAdd.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controller;
 
+use App\DTO\Add;
+use App\Form\AddType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,13 +14,25 @@ class AnnoncementControllerAdd extends AbstractController
      * @Route(
      *     "/add",
      *     name="add",
-     *     methods={"GET", "POST"}
+     *     methods={"GET", "POST"},
+
      * )
      */
-    public function index()
+    public function index(Request $request)
 
     {
+        $add = new Add();
+        $form=$this->createForm(AddType::class, $add);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+    dump($add);die;
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('style/annoncementAdd.html.twig', [
-            'controller_name' => 'AnnoncementControllerAdd']);
+            'controller_name' => 'AnnoncementControllerAdd',
+            'form'=>$form->createView(),
+            ]);
     }
 }
